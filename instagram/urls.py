33e4e8baf75d0b12598e17098182ab9cf8398d2f
Nickghost inc <you@example.com>
2018-10-04 15:54:20 +0300

    Database migrated

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,10 +1,15 @@
+
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.pics, name='insta'),
+    url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
+    url(r'^search/', views.search, name='search')
 
-    ]
+ ]
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
-from .models import Image
+from .models import Image, Profile
+from django.contrib.auth.models import User
 
 # Create your views here.
 def insta(request):
@@ -15,3 +16,27 @@ def pics(request):
 
 # @login_required(login_url='/accounts/login/')
 # def insta(request, article_id):
+
+def search(request):
+    if 'search' in request.GET and request.GET['search']:
+        search_term = request.GET.get('search')
+        profiles = Profile.search_profile(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html',{'message':message, 'profiles':profiles})
+    else:
+        message = 'Enter term to search'
+        return render(request, 'search.html', {'message':message})
+
+
+def profile(request, username):
+    profile = User.objects.get(username=username)
+    # print(profile.id)
+    try:
+        profile_details = Profile.get_by_id(profile.id)
+    except:
+        profile_details = Profile.filter_by_id(profile.id)
+    images = Image.get_profile_images(profile.id)
+    title = f'@{profile.username} Instagram photos and videos'
+
+    return render(request, 'profile/profile.html', {'title':title, 'profile':profile, 'profile_details':profile_details, 'images':images})
