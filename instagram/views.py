68f33f7e0895e083386da236d0f6ff3dbdfc8dd0
Nickghost inc <you@example.com>
2018-10-09 09:16:15 +0300

    Deployment

--- instagram/views.py
+++ instagram/views.py
@@ -88,9 +88,7 @@ def signup(request):
                 user = form.save(commit=False)
                 user.is_active = False
                 user.save()
-                current_site = get_current_site(request)
-                to_email = form.cleaned_data.get('email')
-                send_activation_email(user, current_site, to_email)
+
                 return HttpResponse('Confirm your email address to complete registration')
         else:
             form = SignupForm()
